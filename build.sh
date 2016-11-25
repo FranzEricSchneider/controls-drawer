ca679128c3fa185926cf7355096ce977d4d22448
Eric Schneider <franz.eric.schneider@gmail.com>
2016-11-25 17:07:15 -0500

    Python files now accessible everywhere

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
-source env.sh
-
-cd ${CNC_DRAWER_SOFTWARE}/messages/
-lcm-gen -p lcmtypes_image_t.lcm
-lcm-gen -p lcmtypes_image_request_t.lcm
-lcm-gen -p lcmtypes_velocity_t.lcm
-echo "Built LCM messages in $(pwd)"
-cd ${CNC_DRAWER_BASE}/
+# Must source env.sh first
+if [ -z ${CNC_DRAWER_SOFTWARE+x} ]; then
+	echo "MUST SOURCE ENV.SH BEFORE RUNNING THIS";
+else
+	# Build the LCM messages
+	cd ${CNC_DRAWER_SOFTWARE}/messages
+	lcm-gen -p lcmtypes_image_t.lcm
+	lcm-gen -p lcmtypes_image_request_t.lcm
+	lcm-gen -p lcmtypes_velocity_t.lcm
+	echo "Built LCM messages in $(pwd)"
+	cd ${CNC_DRAWER_BASE}
+fi
--- env.sh
+++ env.sh
@@ -1,5 +1,9 @@
-export CNC_DRAWER_BASE=$(pwd)
+# Make the basic folders that will be built off later
+export CNC_DRAWER_BASE=/home/eon-alone/projects/controls-drawer
 export CNC_DRAWER_SOFTWARE=${CNC_DRAWER_BASE}/software
-
 echo "CNC_DRAWER_BASE = ${CNC_DRAWER_BASE}"
 echo "CNC_DRAWER_SOFTWARE = ${CNC_DRAWER_SOFTWARE}"
+
+# Let the Python files find each other
+export PYTHONPATH=${CNC_DRAWER_SOFTWARE}:${PYTHONPATH}
+export PYTHONPATH=${CNC_DRAWER_SOFTWARE}/messages:${PYTHONPATH}
