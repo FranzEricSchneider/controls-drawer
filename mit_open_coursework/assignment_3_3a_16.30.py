362833c804fa98c32514ba10279848837328465d
Eric Schneider <franz.eric.schneider@gmail.com>
2016-11-29 07:43:35 -0500

    Didnt get correct answer but done

--- mit_open_coursework/assignment_3_3a_16.30.py
+++ mit_open_coursework/assignment_3_3a_16.30.py
@@ -15,10 +15,6 @@ xe1 = gamma
 xe2 = v
 ue = n
 
-# This is the overall gain of the system. It started out as 1 and then I tried
-#   different gain values
-K = 1
-
 A11 = np.sin(xe1) * xe2 / g  # Upper left
 A12 = (-np.cos(xe1) / g) - (ue * g / np.power(xe2, 2))  # Upper right
 A21 = -np.cos(xe1) / g  # Lower left
@@ -28,11 +24,11 @@ B1 = g / xe2
 B2 = -2 * k1 * ue * g / np.power(xe2, 2)
 
 
-X1_over_U_numerator = [K * B1, K * (A12 * B2 - A22 * B1)]
+X1_over_U_numerator = [B1, (A12 * B2 - A22 * B1)]
 X1_over_U_denominator = [1, -(A22 + A11), A22 * A11 - A12 * A21]
 H1 = cnt.tf(X1_over_U_numerator, X1_over_U_denominator)
 
-X2_over_U_numerator = [K * B2, K * (A21 * B1 - A11 * B2)]
+X2_over_U_numerator = [B2, (A21 * B1 - A11 * B2)]
 X2_over_U_denominator = [1, -(A22 + A11), A22 * A11 - A12 * A21]
 H2 = cnt.tf(X2_over_U_numerator, X2_over_U_denominator)
 
@@ -42,8 +38,8 @@ C = A22 * A11 - A12 * A21
 print((-B + np.sqrt(B**2 - 4*A*C)) / 2*A)
 print((-B - np.sqrt(B**2 - 4*A*C)) / 2*A)
 
-cnt.rlocus(H1);
+cnt.rlocus(H1 / (1 + H1));
 plt.show()
 
-cnt.rlocus(H2);
+cnt.rlocus(H2 / (1 + H2));
 plt.show()
