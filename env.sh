3b84d2fd5aa165304faad9ec89c95de1e951a080
Eric Schneider <eric@righthandrobotics.com>
2017-07-15 16:41:15 -0400

    Make work at work

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 # Make the basic folders that will be built off later
-export CNC_DRAWER_BASE=/home/eon-alone/projects/controls-drawer
+export CNC_DRAWER_BASE=~/projects/controls-drawer
 export CNC_DRAWER_SOFTWARE=${CNC_DRAWER_BASE}/software
 export CNC_DRAWER_RESULTS=${CNC_DRAWER_BASE}/results
 echo "CNC_DRAWER_BASE = ${CNC_DRAWER_BASE}"
@@ -9,3 +9,4 @@ echo "CNC_DRAWER_RESULTS = ${CNC_DRAWER_RESULTS}"
 # Let the Python files find each other
 export PYTHONPATH=${CNC_DRAWER_SOFTWARE}:${PYTHONPATH}
 export PYTHONPATH=${CNC_DRAWER_SOFTWARE}/messages:${PYTHONPATH}
+echo "PYTHONPATH = ${PYTHONPATH}"
